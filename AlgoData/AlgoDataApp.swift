7b885d62140769fc849413bd14cff9d3f563da5a
AndreFoulem <timmycasual@icloud.com>
2023-02-23 10:51:03 -0800

    Initial Commit

---
+++ AlgoData/AlgoDataApp.swift
@@ -0,0 +1,17 @@
+//
+//  AlgoDataApp.swift
+//  AlgoData
+//
+//  Created by AndreMacBook on 2023-02-23.
+//
+
+import SwiftUI
+
+@main
+struct AlgoDataApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
